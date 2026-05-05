c3f57699bc6009356c18cd75aee45bff8050925a
Xeninon <alecdscout@gmail.com>
2026-05-05 02:44:15 -0500

    rabbitmq boilerplate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connestion, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal("Error connecting to rabbitmq")
+	}
+	defer connestion.Close()
+
+	fmt.Println("Connection successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down")
 }
